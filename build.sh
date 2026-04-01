e4d9642b68f92d997ac5e3ae195721cf3df6d63d
Meetraj1456 <chavdamitraj1456@gmail.com>
2026-04-01 12:12:34 +0530

    added build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Applying migrations..."
+python manage.py migrate
